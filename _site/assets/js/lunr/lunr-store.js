461226a453d4434faeccf5646b67fb81f36af438
Gary Liu <kll482@nyu.edu>
2020-04-19 23:04:18 +0800

    webp plugin

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "A Crowdfunding Campaign You Can’t Refuse",
         "excerpt":"Introduction Paper Link In this work, we aim to increase the chance of success for the Kickstarter project owners. We collect three datasets: product profiles of Kickstarter, tweets on Twitter and product information from Amazon. Analytic and machine learning models are built upon these datasets to provide insights for project...","categories": [],
         "tags": [],
-        "url": "/portfolio/bdad/",
-        "teaser": "/images/bdad/money.jpg"
+        "url": "http://localhost:4000/portfolio/bdad/",
+        "teaser": "http://localhost:4000/images/bdad/money.jpg"
       },{
         "title": "Predicting the Outcome of Professional Basketball Game",
         "excerpt":"Introduction Paper Link The sports of basketball fascinates us for its unpredictability, but whenever we look retrospectively, the result almost always makes sense. We can explain a victory by many factors: better star players, better team chemistry, better coaching, better momentum, and the list goes on and on. With the...","categories": [],
         "tags": [],
-        "url": "/portfolio/introds/",
-        "teaser": "/images/introds/basketball.jpg"
+        "url": "http://localhost:4000/portfolio/introds/",
+        "teaser": "http://localhost:4000/images/introds/basketball.jpg"
       },{
         "title": "Steps of a NLP Project",
         "excerpt":"Introduction Nowadays, it’s very easy to obtain a series of text data. For instance, Twitter API provides you a short path to scrape people’s posts on Twitter. Even so, you may wonder how do you deal with the Tweets. How do you start a NLP project? However, Natural Language Processing...","categories": [],
         "tags": ["natural language processing","data science"],
-        "url": "/nlpsteps/",
+        "url": "http://localhost:4000/nlpsteps/",
         "teaser": null
       }]
